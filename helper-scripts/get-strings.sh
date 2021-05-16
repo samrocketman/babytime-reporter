c0af1caab759192819cafa93b8df83a7e5bec9d5
Sam Gleske <sam.mxracer@gmail.com>
2021-05-15 23:44:31 -0400

    Update script comment

--- helper-scripts/get-strings.sh
+++ helper-scripts/get-strings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# DESCRIPTION:
+#     I used this once to help me create JSON string entries for the
+#     babytime-telegraf.conf file.  It felt like a waste not to share it.  Then
+#     again, it's kind of a hack.
+
 # USAGE:
 #     ./process_activity_txt.py *.txt | ./helper-scripts/get-strings.sh
 
